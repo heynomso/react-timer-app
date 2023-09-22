3c1ee4eb6355aeaff206e37b9168b484b874e2f7
Nomso <nomsounaogu@gmail.com>
2023-09-22 20:02:56 +0100

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: 'src/main.jsx', 
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: 'src/main.jsx', 
+  //   },
+  // },
   plugins: [react()],
   base: '/react-timer-app/', 
   server: {
