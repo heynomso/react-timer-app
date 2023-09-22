5379933c7efaa9b136e70d24388332374d45b94a
Nomso <nomsounaogu@gmail.com>
2023-09-22 19:57:02 +0100

    modified:   package-lock.json modified:   package.json modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    rollupOptions: {
+      input: 'src/main.jsx', // Adjust the path as needed
+    },
+  },
   plugins: [react()],
   base: '/react-timer-app/',
   server: {
